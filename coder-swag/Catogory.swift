ef23a8d4195727228264be1ee4f9a84948f52bb2
Murathan Bagdat <murathanbagdat@hotmail.com>
2017-08-18 13:44:44 +0300

    Initial Commit

---
+++ coder-swag/Catogory.swift
@@ -0,0 +1,24 @@
+//
+//  Catogory.swift
+//  coder-swag
+//
+//  Created by Melisa Kısacık on 18.08.2017.
+//  Copyright © 2017 MurathanBagdat. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct Catogory {
+    
+    var title : String
+    var imageName : String
+    
+    init(title : String, imageName : String) {
+        self.title = title
+        self.imageName = imageName
+    }
+    
+}
+
---
+++ coder-swag/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Melisa Kısacık on 18.08.2017.
+//  Copyright © 2017 MurathanBagdat. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController  {
+    
+    
+    
+    var catogries = [Catogory]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+// 
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+//        
+//        
+//        return 5
+//    }
+    
+ 
+    
+
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+//        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as
+//        
+//        
+//        return
+//        
+//    }
+
+}
+
